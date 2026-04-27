6ae2599d944042ada511dd59970ca092de9aa37e
Simon Emms <simon@simonemms.com>
2026-04-27 19:13:20 +0000

    feat: add mortgage application domain models and events
    Signed-off-by: Simon Emms <simon@simonemms.com>

---
+++ apps/worker/internal/mortgage/activities/payloads.go
@@ -0,0 +1,54 @@
+package activities
+
+import "time"
+
+type IntakeInput struct {
+	ApplicationID string `json:"applicationId"`
+	ApplicantName string `json:"applicantName"`
+}
+
+type IntakeResult struct {
+	ApplicationID string    `json:"applicationId"`
+	ReceivedAt    time.Time `json:"receivedAt"`
+}
+
+type CreditCheckInput struct {
+	ApplicationID string `json:"applicationId"`
+}
+
+type CreditCheckOutput struct {
+	ApplicationID string    `json:"applicationId"`
+	Result        string    `json:"result"`
+	Reference     string    `json:"reference,omitempty"`
+	CompletedAt   time.Time `json:"completedAt"`
+}
+
+type ReserveOfferInput struct {
+	ApplicationID string `json:"applicationId"`
+}
+
+type ReserveOfferResult struct {
+	ApplicationID string    `json:"applicationId"`
+	OfferID       string    `json:"offerId"`
+	ReservedAt    time.Time `json:"reservedAt"`
+}
+
+type FulfilmentInput struct {
+	ApplicationID string `json:"applicationId"`
+	OfferID       string `json:"offerId"`
+}
+
+type FulfilmentResult struct {
+	ApplicationID string    `json:"applicationId"`
+	FulfilledAt   time.Time `json:"fulfilledAt"`
+}
+
+type ReleaseOfferInput struct {
+	ApplicationID string `json:"applicationId"`
+	OfferID       string `json:"offerId"`
+}
+
+type ReleaseOfferResult struct {
+	ApplicationID string    `json:"applicationId"`
+	ReleasedAt    time.Time `json:"releasedAt"`
+}
