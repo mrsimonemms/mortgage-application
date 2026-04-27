6d0532a87b6f3d974316d0ab51df4450b139c897
Simon Emms <simon@simonemms.com>
2026-04-27 16:59:45 +0100

    feat(ui): initial commit
    Signed-off-by: Simon Emms <simon@simonemms.com>

---
+++ apps/ui/vite.config.ts
@@ -0,0 +1,10 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [sveltekit()],
+  server: {
+    host: process.env.HOST ?? '0.0.0.0',
+    port: Number(process.env.PORT ?? 5173),
+  },
+});
