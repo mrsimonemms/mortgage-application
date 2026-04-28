346736516196f8c563f1407a8c7ece6a3990407b
Simon Emms <simon@simonemms.com>
2026-04-28 15:24:27 +0000

    feat: encrypt temporal calls
    Signed-off-by: Simon Emms <simon@simonemms.com>

--- apps/api/src/config/temporal.ts
+++ apps/api/src/config/temporal.ts
@@ -3,13 +3,16 @@ import { ConnectionOptions } from '@temporalio/client';
 
 export default registerAs(
   'temporal',
-  (): ConnectionOptions => ({
-    address: process.env.TEMPORAL_ADDRESS,
-    apiKey: process.env.TEMPORAL_KEY,
-    connectTimeout: process.env.TEMPORAL_CONNECTION_TIMEOUT,
-    tls: process.env.TEMPORAL_TLS === 'true',
-    metadata: {
-      'temporal-namespace': process.env.TEMPORAL_NAMESPACE ?? 'default',
+  (): { connection: ConnectionOptions; encryptionKeyPath?: string } => ({
+    connection: {
+      address: process.env.TEMPORAL_ADDRESS,
+      apiKey: process.env.TEMPORAL_KEY,
+      connectTimeout: process.env.TEMPORAL_CONNECTION_TIMEOUT,
+      tls: process.env.TEMPORAL_TLS === 'true',
+      metadata: {
+        'temporal-namespace': process.env.TEMPORAL_NAMESPACE ?? 'default',
+      },
     },
+    encryptionKeyPath: process.env.KEYS_PATH,
   }),
 );
